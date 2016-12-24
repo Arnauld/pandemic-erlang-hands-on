a067a01cdcc0e0ceffde2bf9f37903a0a0e411ac
Arnauld <arnauld.loyer@gmail.com>
2016-12-24 23:09:56 +0100

    add json lib for erlang

--- pandemerl/src/pe_web_handler.erl
+++ pandemerl/src/pe_web_handler.erl
@@ -2,9 +2,14 @@
 
 -export([init/2]).
 
+-define(TEXT_PLAIN, <<"text/plain">>).
+
 init(Req0, State) ->
+  	{Method, Req1} = cowboy_req:method(Req0),
+  	{Path,   Req2} = cowboy_req:path(Req1),
+
     Req = cowboy_req:reply(200,
-        #{<<"content-type">> => <<"text/plain">>},
+        #{<<"content-type">> => ?TEXT_PLAIN},
         <<"Hello Erlang!">>,
         Req0),
     {ok, Req, State}.
