825eb6c26d57e70dfa1ff8414f600b62f456966c
Arnauld <arnauld.loyer@gmail.com>
2016-12-26 20:13:03 +0100

    reformat

--- webfront/src/world.jsx
+++ webfront/src/world.jsx
@@ -1,44 +1,48 @@
-import React, {Component} from 'react'
-import config from './config.json';
-import Snap from 'snapsvg-cjs';
+import React, {Component} from "react";
+import config from "./config.json";
+import Snap from "snapsvg-cjs";
 
 class Board extends Component {
-	constructor(props) {
-    	super(props);
-  	}
-  	componentDidMount() {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
         this.updateCanvas();
     }
+
     componentDidUpdate() {
         this.updateCanvas();
     }
+
     updateCanvas() {
         const svg = this.refs.svg;
         const s = Snap(svg);
 
         s.circle(300, 150, 10);
     }
+
     render() {
-		return (
-			<svg ref="svg" width={this.props.width} height={this.props.height}>
-			</svg>
-		);
-	}
+        return (
+            <svg ref="svg" width={this.props.width} height={this.props.height}>
+            </svg>
+        );
+    }
 }
 Board.defaultProps = {
-	width: "900px",
-	height: "636px"
+    width: "900px",
+    height: "636px"
 };
 
-class Greeter extends Component{
-  render() {
-    return (
-      <div>
-        {config.greetText}
-        <Board/>
-      </div>
-    );
-  }
+class Greeter extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{config.greetText}</h1>
+                <Board/>
+            </div>
+        );
+    }
 }
 
 export default Greeter
