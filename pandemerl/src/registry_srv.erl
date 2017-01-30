f198f065b3db0fd128651860eeade4cda0968aa5
Arnauld <arnauld.loyer@gmail.com>
2017-01-30 09:22:35 +0100

    add basic scheduling

--- pandemerl/src/registry_srv.erl
+++ pandemerl/src/registry_srv.erl
@@ -3,6 +3,21 @@
 %%% @copyright (C) 2017, <COMPANY>
 %%% @doc
 %%%
+%%% PROTOCOL:
+%%% =========
+%%%
+%%%
+%%%                      [a:registry]                [x:registry]
+%%%   :synchronize_locals   |                           :
+%%%  ---------------------->||    ∀ n ∈ nodes()         :
+%%%                         ||------------------------->|| {from, from_locals}
+%%%                         ||    :fetch_locals         ||
+%%%                         :                           ||
+%%%                         :          from             ||
+%%%                         ||<-------------------------||
+%%%                              :fetch_locals_response
+%%%
+%%%
 %%% @end
 %%% Created : 18. janv. 2017 19:40
 %%%-------------------------------------------------------------------
@@ -28,7 +43,8 @@
 
 -record(state, {
   locals = [],
-  nodes = []
+  nodes = [],
+  pending_nodes = []
 }).
 
 %%%===================================================================
@@ -38,13 +54,22 @@ start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 register_local({Type, Name}) ->
-  gen_server:cast(?SERVER, {register, Type, Name}).
+  gen_server:call(?SERVER, {register, Type, Name}).
 
 get_locals() ->
   gen_server:call(?SERVER, get_locals).
 
 synchronize_locals() ->
   gen_server:cast(?SERVER, synchronize_locals).
+
+schedule_check_pending_nodes() ->
+  spawn(fun() ->
+    timer:sleep(5 * 1000),
+    check_pending_nodes()
+        end).
+
+check_pending_nodes() ->
+  gen_server:cast(?SERVER, check_pending_nodes).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -56,17 +81,28 @@ init([]) ->
 
 handle_call(get_locals, _From, State) ->
   {reply, State#state.locals, State};
+handle_call({register, Type, Name}, _From, State) ->
+  NewLocals = [{Type, Name}, State#state.locals],
+  NewState = State#state{locals = NewLocals},
+  {reply, ok, NewState};
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
-handle_cast(synchronize_locals, State = #state{locals = Locals}) ->
-  lists:foreach(
-    fun(Node) ->
-      Rep = gen_server:cast({?SERVER, Node}, {fetch_locals, node(), Locals}),
-      error_logger:info_msg("Broadcasting locals to ~p: ~p~n", [Node, Rep])
-    end,
-    nodes()),
+handle_cast(check_pending_nodes, State = #state{pending_nodes = PendingNodes}) ->
+  case PendingNodes of
+    [] ->
+      error_logger:info_msg("No pending node to check~n", []),
+      ok;
+    _ ->
+      broadcast_fetch_locals(State),
+      schedule_check_pending_nodes()
+  end,
   {noreply, State};
+handle_cast(synchronize_locals, State) ->
+  NewState = State#state{pending_nodes = nodes()},
+  broadcast_fetch_locals(NewState),
+  schedule_check_pending_nodes(),
+  {noreply, NewState};
 handle_cast({fetch_locals, Node, NodeLocals}, State = #state{locals = Locals}) ->
   error_logger:info_msg("Received a 'fetch_locals' from node ~p containing: ~p", [Node, NodeLocals]),
   gen_server:cast({?SERVER, Node}, {fetch_locals_response, node(), Locals}),
@@ -76,18 +112,15 @@ handle_cast({fetch_locals_response, Node, NodeLocals}, State) ->
   error_logger:info_msg("Received a 'fetch_locals_response' from node ~p containing: ~p", [Node, NodeLocals]),
   NewState = update_node_locals(State, Node, NodeLocals),
   {noreply, NewState};
-
-handle_cast({register, Type, Name}, State) ->
-  NewLocals = [{Type, Name}, State#state.locals],
-  NewState = State#state{locals = NewLocals},
-  {noreply, NewState};
-
 handle_cast(_Request, State) ->
   {noreply, State}.
 
-handle_info({nodeup, Node}, State) ->
+handle_info({nodeup, Node}, State = #state{pending_nodes = PendingNodes}) ->
   error_logger:info_msg("Node joined: ~p~n", [Node]),
-  {noreply, State};
+  NewPendingNodes = add_node(PendingNodes, Node),
+  NewState = State#state{pending_nodes = NewPendingNodes},
+  schedule_check_pending_nodes(),
+  {noreply, NewState};
 handle_info({nodedown, Node}, State) ->
   error_logger:info_msg("Node left: ~p~n", [Node]),
   NewState = remove_locals_of_node(State, Node),
@@ -107,14 +140,16 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 
 update_node_locals(State, Node, NodeLocals) ->
-  Nodes = State#state.nodes,
+  #state{nodes = Nodes, pending_nodes = PendingNodes} = State,
   NewNodes0 = discard_locals_of_node(Node, Nodes),
   NewNodes1 = append_locals_of_node(Node, NodeLocals, NewNodes0),
-  State#state{nodes = NewNodes1}.
+  NewPendingNodes = remove_node(PendingNodes, Node),
+  State#state{nodes = NewNodes1, pending_nodes = NewPendingNodes}.
 
-remove_locals_of_node(State = #state{nodes = NodesLocals}, NodeToDiscard) ->
+remove_locals_of_node(State = #state{nodes = NodesLocals, pending_nodes = PendingNodes}, NodeToDiscard) ->
   NewNodesLocals = discard_locals_of_node(NodeToDiscard, NodesLocals),
-  State#state{nodes = NewNodesLocals}.
+  NewPendingNodes = remove_node(PendingNodes, NodeToDiscard),
+  State#state{nodes = NewNodesLocals, pending_nodes = NewPendingNodes}.
 
 discard_locals_of_node(NodeToDiscard, NodesLocals) ->
   discard_locals_of_node(NodeToDiscard, NodesLocals, []).
@@ -130,3 +165,35 @@ discard_locals_of_node(NodeToDiscard, [NodeLocal | Others], Acc) ->
 append_locals_of_node(_Node, [], Acc) -> Acc;
 append_locals_of_node(Node, [NodeLocal | Others], Acc) ->
   append_locals_of_node(Node, Others, [{Node, NodeLocal} | Acc]).
+
+broadcast_fetch_locals(#state{locals = Locals, pending_nodes = Nodes}) ->
+  case Nodes of
+    [] ->
+      error_logger:info_msg("No node known to synchronize with", []);
+    _ ->
+      lists:foreach(
+        fun(Node) ->
+          Rep = gen_server:cast({?SERVER, Node}, {fetch_locals, node(), Locals}),
+          error_logger:info_msg("Broadcasting locals to ~p: ~p~n", [Node, Rep])
+        end,
+        Nodes)
+  end.
+
+add_node(Nodes, Node) ->
+  add_node(Nodes, Node, []).
+
+add_node([], Node, Acc) -> [Node | Acc];
+add_node([Node | Others], Node, Acc) ->
+  add_node(Others, Node, Acc);
+add_node([Other | Others], Node, Acc) ->
+  add_node(Others, Node, [Other | Acc]).
+
+
+remove_node(Nodes, Node) ->
+  remove_node(Nodes, Node, []).
+
+remove_node([], _Node, Acc) -> Acc;
+remove_node([Node | Others], Node, Acc) ->
+  remove_node(Others, Node, Acc);
+remove_node([Other | Others], Node, Acc) ->
+  remove_node(Others, Node, [Other | Acc]).
