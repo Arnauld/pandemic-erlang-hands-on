4661da03972045bd48d1ebcd796c78cfd1c6e8fb
Arnauld <arnauld.loyer@gmail.com>
2017-02-20 20:25:33 +0100

    reformat + add bundle path

--- pandemerl/src/pandemerl_app.erl
+++ pandemerl/src/pandemerl_app.erl
@@ -17,49 +17,61 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-	start_web_server(),
-    pandemerl_sup:start_link().
+  start_web_server(),
+  pandemerl_sup:start_link().
 
 %%--------------------------------------------------------------------
 stop(_State) ->
-    ok.
+  ok.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 start_web_server() ->
-	Port = port(),
-	Dispatch = cowboy_router:compile([
-        %% {HostMatch, list({PathMatch, Handler, InitialState})}
-        {'_', [
-            {"/", cowboy_static, {priv_file, ?APP_DIR, "static/index.html"}},
-            {"/assets/[...]", cowboy_static, {priv_dir, ?APP_DIR, "static/assets"}},
-            {"/api/[...]", pe_web_handler, []}]}
-    ]),
+  Port = port(),
+  Dispatch = cowboy_router:compile([
+    %% {HostMatch, list({PathMatch, Handler, InitialState})}
+    {'_', [
+      {"/", cowboy_static, static_index()},
+      {"/assets/[...]", cowboy_static, static_dir()},
+      {"/api/[...]", pe_web_handler, []}]}
+  ]),
 
-    %% Name, NbAcceptors, TransOpts, ProtoOpts
-    {ok, _} = cowboy:start_clear(my_http_listener, 100,
-        [{port, Port}],
-        #{env => #{dispatch => Dispatch}}
-    ),
-    io:format("Http server started on port ~p~n", [Port]).
+  %% Name, NbAcceptors, TransOpts, ProtoOpts
+  {ok, _} = cowboy:start_clear(my_http_listener, 100,
+    [{port, Port}],
+    #{env => #{dispatch => Dispatch}}
+  ),
+  io:format("Http server started on port ~p~n", [Port]).
 
 %%
 %% Retrieve the absolute path of the static resources
 %%
 static_dir() ->
-    {ok, StaticDir} = application:get_env(static_dir),
-    StaticDir.
+  case application:get_env(static_dir) of
+    {ok, StaticDir} ->
+      {dir, StaticDir};
+    _ ->
+      {priv_dir, ?APP_DIR, "static/assets"}
+  end.
+
+static_index() ->
+  case application:get_env(static_dir) of
+    {ok, StaticDir} ->
+      {file, StaticDir ++ "/index.html"};
+    _ ->
+      {priv_file, ?APP_DIR, "static/index.html"}
+  end.
 
 %%
 %% Retrieve the PORT either from an os environment variable
 %% e.g. in Heroku environment, or from the application conf.
 %%
 port() ->
-    case os:getenv("PORT") of
-        false ->
-            {ok, Port} = application:get_env(http_port),
-            Port;
-        Other ->
-            list_to_integer(Other)
-    end.
+  case os:getenv("PORT") of
+    false ->
+      {ok, Port} = application:get_env(http_port),
+      Port;
+    Other ->
+      list_to_integer(Other)
+  end.
