e6ba7b5f8fef3d29f428ad43d8fdd393bbe570fa
Arnauld <arnauld.loyer@gmail.com>
2017-02-22 09:37:06 +0100

    prending infect test

--- pandemerl/test/city_tests.erl
+++ pandemerl/test/city_tests.erl
@@ -14,3 +14,9 @@
 should_be_able_to_create_a_city__test() ->
   City = city:new(london),
   ?assertEqual(london, city:name_of(City)).
+
+should_increase_infection_level_when_infected__test() ->
+  City0 = city:new(london),
+  City1 = city:infect(City0, blue),
+  ?assertEqual(1, city:infection_level(City1, blue)).
+
