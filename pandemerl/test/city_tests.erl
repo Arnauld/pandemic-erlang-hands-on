9fd17a5614a6ba2dcf2b8850614fe8ddaa350c6e
Arnauld <arnauld.loyer@gmail.com>
2017-02-22 20:16:26 +0100

    Because of infected/outbreak: one changes the infect signature to return either {ok, NewCity} | outbreak

--- pandemerl/test/city_tests.erl
+++ pandemerl/test/city_tests.erl
@@ -12,15 +12,24 @@
 -include_lib("eunit/include/eunit.hrl").
 
 should_be_able_to_create_a_city__test() ->
-  City = city:new(london),
+  {ok, City} = city:new(london),
   ?assertEqual(london, city:name_of(City)).
 
 should_not_be_infected_by_default__test() ->
-  City = city:new(london),
+  {ok, City} = city:new(london),
   ?assertEqual(0, city:infection_level(City, blue)).
 
 should_increase_infection_level_when_infected__test() ->
-  City0 = city:new(london),
-  City1 = city:infect(City0, blue),
+  {ok, City0} = city:new(london),
+  {ok, City1} = city:infect(City0, blue),
   ?assertEqual(1, city:infection_level(City1, blue)).
+
+should_outbreak_when_infection_level_reaches_the_threashold__test() ->
+  {ok, City0} = city:new(london),
+  {ok, City1} = city:infect(City0, blue),
+  {ok, City2} = city:infect(City1, blue),
+  {ok, City3} = city:infect(City2, blue),
+  ?assertEqual(3, city:infection_level(City3, blue)),
+  outbreak = city:infect(City3, blue),
+  ?assertEqual(3, city:infection_level(City3, blue)).
 
